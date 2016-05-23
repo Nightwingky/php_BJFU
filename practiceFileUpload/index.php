985244b66bfb75071ba2d6ed9063f92f5e517db9
Nightwingky <qky1024@163.com>
2016-05-23 10:42:58 +0800

    添加了上传文件练习

--- practiceFileUpload/index.php
+++ practiceFileUpload/index.php
@@ -22,9 +22,24 @@
 
 		if($tmp[1] != "doc" && $tmp[1] != "DOC")
 		{
-			
+			header("location:upfile.php?why = 2");
+			exit();
 		}
 
+		if($_FILES["file_name"]["size"] > 40000)
+		{
+			header("location:upfile.php?why = 3");
+			exit();
+		}
+
+		if($_FILES["file_name"]["error"] != 0)
+		{
+			header("location:upfile.php?why = 1");
+			exit();
+		}
+
+		move_uploaded_file($_FILES["file_name"]["tmp_name"], "upfile/a.doc");
+		header("location:upfile.php?why = 4");
 		/*
 		foreach ($_FILE["file_name"] as $key => $value) 
 		{
