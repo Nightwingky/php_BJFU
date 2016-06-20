bfee2b5acbf6e2415042f0f1521a58a25aeda1fc
Nightwingky <qky1024@163.com>
2016-06-20 12:47:44 +0800

    整理了文件夹

--- practiceVerificationCode/index.php
+++ practiceVerificationCode/index.php
@@ -1,88 +1,51 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login</title>
-
-	<style type="text/css">
-		.table
-		{
-			margin: 10px auto;
-		}
-	</style>
+	<title>VerificationCode</title>
 </head>
-
-<script type="text/javascript">
-	function getVerificationCode()
-	{	
-		var alphabet = ["q","w","e","r","t","y","u","i","o","p","o","a","s","d","f","g","h","j","k","l","k","z","x","c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","1","2","3","4","5","6","7","8","9","0"];
-
-		var veriCode = 
-			alphabet[Math.floor(Math.random()*62)]+
-			alphabet[Math.floor(Math.random()*62)]+
-			alphabet[Math.floor(Math.random()*62)]+
-			alphabet[Math.floor(Math.random()*62)];
-
-		//alert(veriCode);
-		/*
-		var httpRequest = new XMLHttpRequest();
-
-  		httpRequest.open("GET","verify.php?veriCode="+veriCode);
-  		httpRequest.send();
-		*/
-  		var yucodeurl = document.getElementById("code").src;
- 		document.getElementById("code").src = yucodeurl +'?'+ veriCode;
-
-  		//document.getElementById("code").innerHTML.src = ;
-	}
-</script>
-
 <body>
 
-<?php
-	//ob_clean();
-	session_start();
+<script type="text/javascript">
 	
-	//header("content-type:image/jpeg");
-?>
+	function changeVerificode()
+	{
+		document.getElementById('capatcha_img').src = 'verificationCode.php?r='+Math.random();
+	}
 
-<table border = "2" class = "table">
-	<tr>
-		<th colspan = "2">Login In</hd>
-	</tr>
-	<tr>
-		<td align = "center">User Name</td>
-		<td><input type = "test" name = "user_name"></td>
-	</tr>
-	<tr>
-		<td align = "center">Password</td>
-		<td><input type = "test" name = "passward"></td>
-	</tr>
-	<tr>
-		<td align = "center">Verification Code</td>
-		<td>
-			<input type = "test" name = "verification">
-		</td>
-	</tr>
-	<tr>
-		<td align = "center">
-			<img width = "100px" height = "25px" src = "verify.php">
-		</td>
-		<td align = "center">
-			<button type = "button" onclick = "getVerificationCode()" id = "code">Another One</button>
-		</td>
-	</tr>
-	<tr>
-		<td align = "center" colspan = "2">
-			<button type = "button">Submit</button>
-			&nbsp;&nbsp;
-			<button type = "button">Reset</button>
-		</td>
-	</tr>
-</table>
+</script>
 
 <?php
-	var_dump($_SESSION);
+
+	if(isset($_REQUEST['authcode']))
+	{
+		session_start();
+
+		if(strtolower($_REQUEST['authcode']) == strtolower($_SESSION['authcode']))
+		{
+			echo '<font color = "#0000CC">correct</font>';
+		}
+		else
+		{
+			echo '<font color = "#CC0000">wrong</font>';
+		}
+		exit();
+	}
 ?>
+
+<form 
+	method = "post"
+	action = "index.php">
+	<p>VerificationCode:
+		<img id = "capatcha_img" border = "1" src = "verificationCode.php?r=<?php echo rand();?>" width = "100" height = "30">
+		<a href = "javascript:void(0)" onclick = "changeVerificode()">Another One?</a>
+	</p>
+	<p>Input:
+		<input type = "text" name = "authcode" value = ""/>
+	</p>
+	<p>
+		<input type = "submit" value = "submit" style = "padding:6px 20px;">
+	</p>
+</form>
 
 </body>
 </html>
